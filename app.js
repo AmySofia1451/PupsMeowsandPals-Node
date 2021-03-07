15f6dced2346d0d51fc66e71b7dcdc7e2883ea48
Amy Sofia <amysofia1451@gmail.com>
2021-03-07 13:16:21 -0500

    after setting up node to push

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 7,
-  center: {
-    lat: 39.821466069992674,
-    lng: -105.11993204533603
-  }
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+
+// Main content starts here
+
+
+// Below, when client goes to the home route, page will run the function to find the Items collection,
+// if it can find then it will delivery the content, if it can't find it will show an error.
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
 });
-map.setOptions({minZoom: 7});
+
+app.get("/policies", function(req, res) {
+  res.sendFile(__dirname + "/policies.html")
+});
+
+app.get("/contact", function(req, res) {
+  res.sendFile(__dirname + "/contact.html")
+});
 
 
-"use strict";
-const nodemailer = require("nodemailer");
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: clientName <email>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// nodemailer
+
+
+
+
+
+
+// HEROKU PORT below
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
 }
 
-main().catch(console.error);
+app.listen(port, function() {
+  console.log("Server started!");
+});
